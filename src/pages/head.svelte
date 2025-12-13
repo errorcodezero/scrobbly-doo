b96c6a85e8f64b43afd3a16e8541c2456d1622af
ErrorCode0 <me@errorcodezero.dev>
2025-12-12 22:47:47 -0800

    seperated the og image stuff and the player itself into seperate components

---
+++ src/pages/head.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Track } from "../lib/last_fm";
+
+	let { track }: { track: Track } = $props();
+</script>
+
+{#if track.defined}
+	<meta property="og:title" content={track.name} />
+	<meta property="og:image" content={track.images.at(1)} />
+	<meta property="og:type" content="music.song" />
+	<meta property="og:music:album" content={track.album} />
+	<meta property="og:music:musician" content={track.artists} />
+{/if}
+<title>{track.name}</title>
